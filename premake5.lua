1a95be05fc7f4f46371ac50fe7d4c7d6ed92c59b
unknown <tgalcik1@umbc.edu>
2023-10-08 19:47:41 -0400

    viewport setup

--- premake5.lua
+++ premake5.lua
@@ -1,10 +1,10 @@
 -- premake5.lua
-workspace "WalnutApp"
+workspace "raytracing"
    architecture "x64"
    configurations { "Debug", "Release", "Dist" }
-   startproject "WalnutApp"
+   startproject "raytracing"
 
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 include "Walnut/WalnutExternal.lua"
 
-include "WalnutApp"
+include "raytracing"
